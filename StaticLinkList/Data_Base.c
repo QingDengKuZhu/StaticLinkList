ea58b03dea0a694dd41a90a2ee1b81bdc805055f
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-06-08 09:10:15 +0800

    静态单链表的初始化

--- StaticLinkList/Data_Base.c
+++ StaticLinkList/Data_Base.c
@@ -1,1 +1,21 @@
-﻿
+﻿#include "Data.h"
+#include "Data_Base.h"
+#include "Main_First.h"
+
+
+void InitList(STATICLINKLIST pL, size_t list_len)
+{
+	/*
+	**第一个元素的cur存放第一个有效结点的下标,最后一个结点的cur存放第一个无效结点的下标
+	*/
+	size_t i;
+	for (i=1; i<=list_len-2; ++i)
+	{
+		pL[i].cur = i+1;
+	}
+
+	pL[0].cur = 0;
+	pL[list_len-1].cur = 1;
+
+	return;
+}
