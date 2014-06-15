93b96001202c222a939383fa40faaed398c0620c
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-06-15 10:51:07 +0800

    代码规范
    所有的变量都必须初始化

--- StaticLinkList/Data_Base.c
+++ StaticLinkList/Data_Base.c
@@ -8,7 +8,7 @@ void InitList(STATICLINKLIST pL, size_t list_len)
 	/*
 	**第一个元素的cur存放第一个有效结点的下标,最后一个结点的cur存放第一个无效结点的下标
 	*/
-	size_t i;
+	size_t i = 0;
 	for (i=1; i<=list_len-2; ++i)
 	{
 		pL[i].cur = i+1;
@@ -18,4 +18,22 @@ void InitList(STATICLINKLIST pL, size_t list_len)
 	pL[list_len-1].cur = 1;
 
 	return;
+}
+
+void TraveList(STATICLINKLIST pL, size_t list_len)
+{
+	size_t pos = pL[0].cur;
+	while (pos != 0 && pos != pL[list_len-1].cur )
+	{
+		printf("%d ", pL[pos].data);
+		pos = pL[pos].cur;
+	}
+	printf("\n");
+
+	return;
+}
+
+size_t ListLength(STATICLINKLIST pL, size_t list_len)
+{
+
 }
